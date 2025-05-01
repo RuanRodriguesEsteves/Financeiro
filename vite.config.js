503e81578350cbc230c978351e5125e03a865689
RRE gameplay <ruan000esteves@gmail.com>
2025-05-01 20:50:02 -0300

    Configuração do Vite para poder acessar os gráficos em outro dispositivo na mesma rede

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: '192.168.3.102' //Para poder acessar em outros dispositivos na rede
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
